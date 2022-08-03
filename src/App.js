d44736721d67275213edb98ce252a61c7905d969
berdanakyurek <berdanakyurek17@gmail.com>
2022-08-03 16:41:28 +0300

    filtering by all, active or closed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { v4 as uuidv4 } from 'uuid';
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 function App() {
     const [todos, setTodos] = useState([]);
+    const [shown, setShown] = useState([]);
+    const [allState, setAllState] = useState(0);
     const todoNameRef = useRef();
     useEffect(()=> {
         const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
@@ -25,6 +27,8 @@ function App() {
             }
             setTodos(newTodos)
         })
+
+        showGeneral()
     }
 
     function countCompleted(arr){
@@ -33,7 +37,54 @@ function App() {
             if(arr[i].completed) cnt ++;
         }
         return cnt;
+
+
     }
+
+    function showAll(){
+        setShown([...todos])
+        setAllState(0);
+    }
+
+    function showActive(){
+        var newTodos = []
+
+        for(var i = 0; i < todos.length; i ++)
+        {
+            if(!todos[i].completed){
+                newTodos.push(todos[i])
+            }
+        }
+        setShown(newTodos)
+        setAllState(1);
+        console.log(allState);
+    }
+
+    function showInactive(){
+        var newTodos = []
+
+        for(var i = 0; i < todos.length; i ++)
+        {
+            if(todos[i].completed){
+                newTodos.push(todos[i])
+            }
+        }
+        setShown(newTodos)
+        setAllState(2);
+    }
+
+    function showGeneral(){
+        if(allState == 0){
+            showAll()
+        }
+        else if(allState == 1){
+            showActive()
+        }
+        else if(allState == 2){
+            showInactive()
+        }
+    }
+
     function handleAddTodo(e){
         const name = todoNameRef.current.value
         if(name === '') return
@@ -41,8 +92,11 @@ function App() {
         newTodos.push({id:uuidv4(), name:name, completed:false});
         //console.log(newTodos)
         setTodos(newTodos);
+        setAllState(0);
+        setShown(newTodos);
         todoNameRef.current.value = ''
     }
+
 
     function clearChecked(e){
         console.log(todos)
@@ -52,13 +106,17 @@ function App() {
                 newTodos.push(todos[i])
             }
         }
-        console.log(newTodos)
         setTodos(newTodos)
     }
 
     return (
         <>
-            <TodoList todos={todos} toggleTodo ={toggle} />
+            <div>
+                <input type="radio" value="All" name="filter" onClick ={showAll} checked={allState==0} /> All
+                <input type="radio" value="Active" name="filter" onClick ={showActive} checked={allState==1} /> Active
+                <input type="radio" value="Completed" name="filter" onClick ={showInactive} checked={allState==2} /> Completed
+            </div>
+            <TodoList todos={shown} toggleTodo ={toggle} defaultChecked />
             <input ref={todoNameRef} type="text" />
             <button onClick ={handleAddTodo}>Add</button>
             <button onClick ={clearChecked}>Clear</button>
